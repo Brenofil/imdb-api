e0dab38c6763386bcedbc8ff4ca584cb18f61d15
Breno Costa da Silva Filgueiras <brenofil@Brenos-MacBook-Pro.local>
2022-08-13 14:36:28 -0300

    brenofil - heroku issues

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+import { ApplicationConfig, ImdbApiApplication } from './application';
+export * from './application';
+export declare function main(options?: ApplicationConfig): Promise<ImdbApiApplication>;
